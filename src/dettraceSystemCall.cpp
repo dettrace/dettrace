f4692939feec16356f0c028a9b2ad3cda5a5a764
Joe Devietti <joe@devietti.net>
2018-02-26 20:32:14 -0500

    ioctl() syscalls for manipulating terminal return ENOTTY now

--- src/dettraceSystemCall.cpp
+++ src/dettraceSystemCall.cpp
@@ -436,7 +436,7 @@ bool ioctlSystemCall::handleDetPre(state &s, ptracer &t){
 void ioctlSystemCall::handleDetPost(state &s, ptracer &t){
   const uint64_t request = t.arg2();
   if (TCGETS == request || TIOCGWINSZ == request) {
-    t.setReturnRegister((uint64_t) -1);
+    t.setReturnRegister((uint64_t) -ENOTTY);
   } else {
     throw runtime_error("Unsupported ioctl call: fd="+to_string(t.arg1())+" request=" + to_string(request));
   }
