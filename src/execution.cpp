8931366f42a28f7c05fbfb89d93ef49b51655072
gatowololo <gatowololo@gmail.com>
2018-07-23 17:33:36 -0400

    Only checks for threads on clone... dumb mistake.

--- src/execution.cpp
+++ src/execution.cpp
@@ -102,13 +102,15 @@ bool execution::handlePreSystemCall(state& currState, const pid_t traceesPid){
   // https://stackoverflow.com/questions/29997244/
   // occasionally-missing-ptrace-event-vfork-when-running-ptrace
   if(systemCall == "fork" || systemCall == "vfork" || systemCall == "clone"){
+    
     // Threads are not supported!
-    unsigned long flags = (unsigned long) tracer.arg1();
-    unsigned long threadBit = flags & CLONE_THREAD;
-    if(threadBit != 0){
-      throw runtime_error("Threads not supported!");
+    if(systemCall == "clone" ){
+      unsigned long flags = (unsigned long) tracer.arg1();
+      unsigned long threadBit = flags & CLONE_THREAD;
+      if(threadBit != 0){
+        throw runtime_error("Threads not supported!");
+      }
     }
-
 
     int status;
     ptraceEvent e;
