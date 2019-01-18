904d87404b565f2fcd7ce31dd217b6abe5c81ece
gatowololo <gatowololo@gmail.com>
2019-01-18 11:53:40 -0500

    Set permissions back to user after package installation.

--- benchmarking/install_package.sh
+++ benchmarking/install_package.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+# This script must run as sudo!!
+# Installs a new package inside chroot under /home/
 
 # Move actual script to chroot.
 cp ./scripts/install_package.sh ./wheezy/install_package.sh
@@ -15,3 +18,7 @@ then
     echo "Installing package"
     chroot ./wheezy /install_package.sh $package
 fi
+
+# Set permissions so user can write to any directory in their chroot.
+chgrp -R $(logname 2>/dev/null || echo $SUDO_USER) $current_dir
+chown -R $(logname 2>/dev/null || echo $SUDO_USER) $current_dir
