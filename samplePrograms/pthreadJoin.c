5752899e1621a32149b59b86ab9c3f758044ecd3
gatowololo <gatowololo@gmail.com>
2019-04-17 15:02:11 -0400

    Add threaded tests (Thanks Kelly!)

---
+++ samplePrograms/pthreadJoin.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+// Process spawns thread with pthread api.
+// Thread blocks trying to read from a pipe.
+// Can the process still exit?
+
+const int bytesToRead = 100;
+const int bytesToWrite = 100;
+int pipefd[2];
+
+int thread_func(void *arg){
+  printf("Thread - my pid is: %d\n", getpid());
+  printf("Thread - my ppid is: %d\n", getppid());
+  printf("Thread wants to read from pipe.\n");
+  
+  char buf[bytesToRead];
+  int bytes = read(pipefd[0], buf, bytesToRead);
+  printf("Thread read this many bytes. %d\n", bytes);
+  fflush(NULL);
+  pthread_exit(NULL);
+}
+
+int main(void){
+  printf("Process - my pid is: %d\n", getpid());
+  printf("Process - my ppid is: %d\n", getppid());
+  int ret = pipe(pipefd);
+  if(ret != 0){
+    printf("Pipe errored.\n");
+    exit(1);
+  }
+
+  pthread_t thread1;
+  int p = pthread_create(&thread1, NULL, thread_func, NULL);
+  printf("Pthread create returned: %d\n", p);
+
+  char buffer[bytesToWrite];
+  int b = write(pipefd[1], buffer, bytesToWrite);
+  printf("Process wrote this many bytes: %d\n", b);
+  printf("Just gonna exit now.\n");
+  
+  pthread_join(thread1, NULL);
+  exit(0);
+}
---
+++ samplePrograms/tenThreadNoJoin.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+// TODO: Thread function.
+void thread_func(void *arg){
+  printf("Thread's pid: %d\n", getpid());
+  printf("Thread's ppid: %d\n", getppid());
+  pthread_exit(NULL);
+}
+
+int main() {
+  pthread_t threads[10];
+  int count;
+  
+  for(count = 0; count < 10; count++){
+    pthread_create(&threads[count], NULL, thread_func, NULL);
+    printf("Creating thread\n");
+  }
+
+  printf("Main process exiting.\n");
+  return 0;
+}
