8803892312af8c4c9c60e40f404d967deb66a64a
gatowololo <gatowololo@gmail.com>
2019-04-20 11:24:34 -0400

    Updated thread tests to work with complexThread

--- samplePrograms/processAndThread.c
+++ samplePrograms/processAndThread.c
@@ -1,4 +1,5 @@
 #define _GNU_SOURCE
+#include <sys/types.h>
 #include <sched.h>
 #include <errno.h>
 #include <stdio.h>
@@ -17,13 +18,14 @@
 const int bytesToRead = 100;
 const int bytesToWrite = 100;
 int pipefd[2];
+
 // Process spawns thread with clone().
 // Thread blocks trying to read from a pipe.
 // Can the process still exit?
 int thread_func(void *arg){
   //sleep(5);
   char buf[bytesToRead];
-  printf("Thread - my pid is: %d\n", getpid());
+  printf("Thread - my tid is: %ld\n", syscall(SYS_gettid));
   printf("Thread - my ppid is: %d\n", getppid());
 
   printf("Thread wants to read from pipe.\n");
@@ -43,8 +45,6 @@ int main(void){
     exit(1);
   }
 
-//  int readEnd = pipefd[0];
-//  int writeEnd = pipefd[1];
 
   printf("Process - my pid is: %d\n", getpid());
   printf("Process - my ppid is: %d\n", getppid());
@@ -52,11 +52,14 @@ int main(void){
   void* child_stack = malloc(STACK_SIZE);
   int thread_pid;
   printf("Creating new thread.\n");
-  //thread_pid = clone(thread_func, child_stack+STACK_SIZE, CLONE_VM|CLONE_FS|CLONE_FILES|CLONE_SIGHAND|CLONE_THREAD|SIGCHLD, NULL);
-  thread_pid = clone(thread_func, child_stack+STACK_SIZE, CLONE_VM|CLONE_FS|CLONE_FILES|CLONE_SIGHAND|CLONE_THREAD|CLONE_PARENT_SETTID|CLONE_CHILD_CLEARTID|SIGCHLD, NULL);
-  printf("Done! Thread pid: %d\n", thread_pid);
+
+  thread_pid = clone(thread_func, child_stack+STACK_SIZE, CLONE_VM|CLONE_FS|CLONE_FILES|CLONE_SIGHAND|CLONE_THREAD|CLONE_PARENT_SETTID| SIGCHLD, NULL);
+  printf("Thread created! Thread pid: %d\n", thread_pid);
 
   char buffer[bytesToWrite];
+  for (int i = 0; i < bytesToWrite; i++) {
+    buffer[i] = 0;
+  }
   int b = write(pipefd[1], buffer, bytesToWrite);
   printf("Process just wrote this many bytes: %d\n", b);
   printf("Just gonna exit now.\n");
--- samplePrograms/processThreadThread.c
+++ samplePrograms/processThreadThread.c
@@ -12,35 +12,36 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
+// Process spawns thread which spawns thread.
+// Can thread2 exit before thread one, the answer is yes.
 #define STACK_SIZE (1024 * 1024)
 
 const int bytesToRead = 100;
 const int bytesToWrite = 100;
 int pipefd[2];
 
-int thread_func(void *arg){
-  printf("Thread1 forking P2.\n");
-  printf("Thread 1 pid: %d\n", getpid());
-  printf("Thread 1 ppid: %d\n", getppid());
-  pid_t p2 = fork();
-  if (p2 == 0){
-    // Child process tries to read from pipe.
-    printf("Child process of thread trying to read from pipe.\n");
-    printf("Child process pid: %d\n", getpid());
-    printf("Child process' parent pid: %d\n", getppid());
-    char buf[bytesToRead];
-    int bytes = read(pipefd[0], buf, bytesToRead);
-    printf("Child process read this many bytes: %d\n", bytes);
-    printf("Child process done.\n");
-    printf("Child process parent pid is now: %d\n", getppid());
-    return 0;
-  }
-  
-  // Thread writes to pipe.
+int second_thread(void *arg){
+  printf("Thread2 trying to read from pipe.\n");
+  printf("my pid is: %d\n", getpid());
+  char buf[bytesToRead];
+  int bytes = read(pipefd[0], buf, bytesToRead);
+  printf("Thread2 read this many bytes: %d\n", bytes);
+  printf("Thread2 is done.\n");
+  fflush(NULL);
+  return 0;
+}
+
+int first_thread(void *arg){
+  void* thread2_stack = malloc(STACK_SIZE);
+  printf("Thread1 cloning thread2.\n");
+  int thread_pid2;
+  thread_pid2 = clone(second_thread, thread2_stack+STACK_SIZE, CLONE_VM|CLONE_FS|CLONE_FILES|CLONE_SIGHAND|CLONE_THREAD, NULL);
+  printf("Thread1 cloned thread2, pid: %d\n", thread_pid2);
+
   char buffer[bytesToWrite];
   int b = write(pipefd[1], buffer, bytesToWrite);
-  printf("Thread1 wrote this many bytes: %d\n", b); 
-  printf("Thread1 done, returning.\n");
+  printf("Thread1 wrote this many bytes: %d\n", b);
+  printf("Thread1 is done.\n");
   fflush(NULL);
   return 0;
 }
@@ -51,11 +52,11 @@ int main(void){
     printf("Pipe errored.\n");
     exit(1);
   }
-  printf("Parent process pid: %d\n", getpid());
-  printf("Parent process ppid: %d\n", getppid());
+
   void* child_stack = malloc(STACK_SIZE);
   int thread_pid;
-  thread_pid = clone(thread_func, child_stack+STACK_SIZE, CLONE_VM|CLONE_FS|CLONE_FILES|CLONE_SIGHAND|CLONE_THREAD, NULL);
+  printf("Process cloning thread1.\n");
+  thread_pid = clone(first_thread, child_stack+STACK_SIZE, CLONE_VM|CLONE_FS|CLONE_FILES|CLONE_SIGHAND|CLONE_THREAD, NULL);
   printf("Process made thread1, pid: %d\n", thread_pid);
   printf("Process is exiting.\n");
   exit(EXIT_SUCCESS);
