414a15a982d97651ab92a43901f191f301b2a794
gatowololo <gatowololo@gmail.com>
2019-05-09 14:47:24 -0400

    Use timeout.py

--- findBug.sh
+++ findBug.sh
@@ -2,7 +2,8 @@
 
 while true
 do
-    ./bin/dettrace --debug 4 test/samplePrograms/twoPthreadsNoJoin.bin 2> temp.txt > /dev/null
+    echo "First Try..."
+    python3 ./test/samplePrograms/timeout.py 5s ./bin/dettrace --debug 4 test/samplePrograms/twoPthreadsNoJoin.bin 2> temp.txt > /dev/null
     rc=$?;
     if [[ $rc != 0 ]]; then more temp.txt; exit $rc; fi
 done
