ad16f2e89998a4d3daf5433217235df0c19ab76e
gatowololo <gatowololo@gmail.com>
2019-05-09 15:16:54 -0400

    Nothing makes sense...

--- findBug.sh
+++ findBug.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-while true
-do
-    echo "First Try..."
-    ./bin/dettrace --debug 4 test/samplePrograms/twoPthreadsNoJoin.bin
-    rc=$?;
-    if [[ $rc != 0 ]]; then more temp.txt; exit $rc; fi
-done
+echo "First Try..."
+<bin/dettrace --debug 4 test/samplePrograms/twoPthreadsNoJoin.bin
+
 
