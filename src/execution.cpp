92cfe1763c9b15c4c8b2f297f4ed79df88bbeb63
Joe Devietti <devietti@cis.upenn.edu>
2019-08-06 21:34:40 -0400

    richer CPUID emulation

--- src/execution.cpp
+++ src/execution.cpp
@@ -844,10 +844,10 @@ void execution::handleSignal(int sigNum, const pid_t traceesPid){
         //tracer.writeRcx( 0x6c6c6c6c ); // for debugging, returns "GenuineIllll" instead
         break;
       case 0x01: // basic features
-        tracer.writeRax( 0x0 );
-        tracer.writeRbx( 0x0 );
-        tracer.writeRdx( 0x0 );
-        tracer.writeRcx( 0x0 );
+        tracer.writeRax( 0x306c3 );
+        tracer.writeRbx( 0x4100800 );
+        tracer.writeRdx( 0xbfebfbff );
+        tracer.writeRcx( 0x7ffafbff );
         break;
       case 0x02: // TLB/Cache/Prefetch Information
         // say that we have no caches, TLBs or prefetchers
@@ -868,11 +868,36 @@ void execution::handleSignal(int sigNum, const pid_t traceesPid){
         tracer.writeRdx( 0x0 );
         tracer.writeRcx( 0x0 );
         break;
+      case 0x07:
+        tracer.writeRax( 0x0 );
+        tracer.writeRbx( 0x0 );
+        tracer.writeRdx( (1ul << 10) | /* MD_CLEAR */
+                         (1ul << 26) | /* IBRS/IBPB */
+                         (1ul << 27) | /* STIBP */
+                         (1ul << 28) | /* L1D_FLUSH */
+                         (1ul << 29) | /* IA32_ARCH_CAPABILITIES */
+                         (1ul << 31) | /* SSBD */
+                         0 );
+        tracer.writeRcx( 0x0 );
+        break;
       case 0x80000000:
         tracer.writeRax( 0x80000000 );
         tracer.writeRbx( 0x0 );
         tracer.writeRdx( 0x0 );
         tracer.writeRcx( 0x0 );
+        break;
+      case 0x80000001:
+        tracer.writeRax( 0x0 );
+        tracer.writeRbx( 0x0 );
+        tracer.writeRcx( 0x21 );
+        tracer.writeRdx( 0x2c100800 );
+        break;
+      case 0x80000006: // L2 cache
+        tracer.writeRax( 0x0 );
+        tracer.writeRbx( 0x0 );
+        tracer.writeRdx( 0x0 );
+        // size=2MB, 16-way set associative, line size = 64B
+        tracer.writeRcx( 0x08008140 );
         break;
       default:
         runtimeError("CPUID unsupported %eax argument");
