5ec47ce1a76f6fa26f0fb7a46b9eb38eedbd4072
Jason White <github@jasonwhite.io>
2019-09-12 12:23:04 -0700

    Clean up and generalize build scripts
    This adds `make env` for creating a Docker-based development
    environment.
    
    This also allows changing the executable name with `make NAME=foobar`.

---
+++ ci/create_deb.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+# Creates a Debian package.
+
+set -e
+
+NAME=$1
+VERSION=$2
+
+PKGNAME=${NAME}_${VERSION}
+
+cleanup() {
+    # Delete the package staging directory
+    rm -rf -- "${PKGNAME}"
+}
+
+cleanup
+
+trap cleanup EXIT
+
+mkdir -p -- "${PKGNAME}/usr/bin"
+mkdir -p -- "${PKGNAME}/usr/share/${NAME}/bin"
+
+cp -a bin/${NAME}-static "${PKGNAME}/usr/share/${NAME}/bin/${NAME}"
+ln -sf "../share/${NAME}/bin/${NAME}" "${PKGNAME}/usr/bin/${NAME}"
+cp -a root "${PKGNAME}/usr/share/${NAME}/root"
+find "${PKGNAME}/usr/share/${NAME}/root" -type f -name .gitignore -exec rm '{}' \+
+cp -a initramfs.cpio "${PKGNAME}/usr/share/${NAME}/"
+cp -a examples "${PKGNAME}/usr/share/${NAME}/"
+mkdir -- "${PKGNAME}/DEBIAN"
+
+cat > "${PKGNAME}/DEBIAN/control" << EOF
+Package: ${NAME}
+Version: ${VERSION}
+Section: base
+Priority: optional
+Architecture: amd64
+Depends:
+Maintainer: Jason White
+Description: ${NAME}
+ Runs programs deterministically using ptrace.
+EOF
+
+fakeroot -- dpkg-deb --build "${PKGNAME}"
---
+++ examples/rand.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+import random
+for x in range(10):
+  print(random.randint(1,101),end=" ")
+print("")
+
+
