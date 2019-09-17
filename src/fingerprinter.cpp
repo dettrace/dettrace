1434bdf973853357a911b3d885d1aa55dbd60548
Logan Wendholt <lwendholt@gmail.com>
2019-09-17 15:49:40 -0400

    Remove reference to captured_syscall

--- src/fingerprinter.cpp
+++ src/fingerprinter.cpp
@@ -30,17 +30,3 @@ void fingerprinter::callPostHook(int syscallNumber, globalState& gs, state& s, p
     &syscallState, s.traceePid, s.traceePid, syscallNumber, (long)regs.rax, regs.rdi,
     regs.rsi, regs.rdx, regs.r10, regs.r8, regs.r9);
 }
-
-// Implementation is required due to captured_syscall being exposed from lib,
-// even though it's not used here in DetTrace
-extern "C" long untraced_syscall(
-  int no,
-  unsigned long a0,
-  unsigned long a1,
-  unsigned long a2,
-  unsigned long a3,
-  unsigned long a4,
-  unsigned long a5
-) {
-  return 0;
-}
