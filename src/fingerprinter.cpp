3e467e71fec98bd864aa83e5da07b6ffdf316f84
Logan Wendholt <lwendholt@gmail.com>
2019-11-08 11:45:29 -0500

    Fix noop behavior for fingerprinter

--- src/fingerprinter.cpp
+++ src/fingerprinter.cpp
@@ -5,18 +5,26 @@ using namespace std;
 
 // TODO: Currently passing PID into prehook and posthook for both PID and TIG arguments. Fix this.
 
+// Fingerprinter State
+static bool isNoop = false;
+static int noopSyscall = -1;
+static long noopRetval = -1;
+
 bool fingerprinter::callPreHook(int syscallNumber, globalState& gs, state& s, ptracer& t, scheduler& sched){
   struct SyscallState syscallState;
   syscallState.noop = false;
   auto regs = t.getRegs();
-  auto prehook_retval = fingerprinter_prehook(
+  long prehook_retval = fingerprinter_prehook(
     &syscallState, s.traceePid, s.traceePid, syscallNumber, regs.rdi,
     regs.rsi, regs.rdx, regs.r10, regs.r8,  regs.r9);
   // If fingerprinter indicates that the syscall shouldn't be run,
   // cancel the syscall and set the return value
   if(syscallState.noop) {
-    cancelSystemCall(gs, s, t);
-    t.writeRax((uint64_t)prehook_retval);
+    isNoop = true;
+    noopSyscall = syscallNumber;
+    noopRetval = prehook_retval;
+    replaceSystemCallWithNoop(gs, s, t);
+    t.setReturnRegister((uint64_t)prehook_retval);
   }
   // Return flag indicating whether to run post-hook
   return true;
@@ -25,8 +33,13 @@ bool fingerprinter::callPreHook(int syscallNumber, globalState& gs, state& s, pt
 void fingerprinter::callPostHook(int syscallNumber, globalState& gs, state& s, ptracer& t, scheduler& sched){
   struct SyscallState syscallState;
   syscallState.noop = false;
+  if(isNoop) {
+    t.changeSystemCall((uint64_t)noopSyscall);
+    t.setReturnRegister((uint64_t)noopRetval);
+    isNoop = false;
+  }
   auto regs = t.getRegs();
   fingerprinter_posthook(
-    &syscallState, s.traceePid, s.traceePid, syscallNumber, (long)regs.rax, regs.rdi,
+    &syscallState, s.traceePid, s.traceePid, regs.orig_rax, (long)regs.rax, regs.rdi,
     regs.rsi, regs.rdx, regs.r10, regs.r8, regs.r9);
 }
