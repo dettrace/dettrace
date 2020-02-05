4f541d04b2fbcb47faff83051b342f7b3086eae4
Ryan Rhodes Newton <newton@fb.com>
2020-02-05 16:26:56 -0500

    Update expected hardcoded time numbers -- redux

--- test/unitTests/systemCallTests.cpp
+++ test/unitTests/systemCallTests.cpp
@@ -245,7 +245,7 @@ TEST_CASE("utime", "utime"){
   REQUIRE(myStat.st_atim.tv_sec == 744847200);
   REQUIRE(myStat.st_atim.tv_nsec == 0);
   REQUIRE(myStat.st_mtim.tv_sec == 744847200);
-  REQUIRE(myStat.st_mtim.tv_nsec == 0);
+  REQUIRE(myStat.st_mtim.tv_nsec == 33000);
 }
 
 TEST_CASE("uid/gid", "uid/gid"){
